1893c59d5fde50a0528c291212af54432b2fd945
Pavel <paha-r@yandex.ru>
2020-10-09 14:12:31 +0300

    Task 1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -10,6 +10,29 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            //Родыгин
+            //Написать программу «Анкета». 
+            //Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес).
+            //В результате вся информация выводится в одну строчку:
+            //а) используя склеивание;
+            //б) используя форматированный вывод;
+            //в) используя вывод со знаком $.
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.Write("Введите рост: ");
+            short height = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Введите вес: ");
+            short weight = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(name + " " + surname + " " + age + " " + height + " " + weight);
+            Console.WriteLine("{0} {1} {2} {3} {4}", name, surname, age, height, weight);
+            Console.WriteLine($"{name} {surname} {age} {height} {weight}");
+
+            Console.ReadKey();
         }
     }
 }
