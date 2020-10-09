b2ab8c3e57f5628fb18c69118839d301bc407dd7
Pavel <paha-r@yandex.ru>
2020-10-09 14:20:04 +0300

    Task 6

---
+++ Lesson1/Task6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+            ShortCom.Print("Hello, world!");
+            ShortCom.Print(15);
+            ShortCom.Pause();
+        }
+    }
+
+    class ShortCom
+    {
+        public static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void Print(int number)
+        {
+            Console.WriteLine(number);
+        }
+
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+    }
+}
