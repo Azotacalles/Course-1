a6753723514dbf773b55837957e8ea7a1e8bdb4c
Pavel <paha-r@yandex.ru>
2020-10-15 22:06:03 +0300

    Task2 begin

---
+++ Homework3/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Родыгин
+            //а)  С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). 
+            //Требуется подсчитать сумму всех нечётных положительных чисел. 
+            //Сами числа и сумму вывести на экран, используя tryParse.
+
+            Console.ReadKey();
+        }
+    }
+}
